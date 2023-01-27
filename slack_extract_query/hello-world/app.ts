b3c756678e30ae3da90fc278226b1b72f6e9bcf1
ddunford <48522654+darrendunford@users.noreply.github.com>
2023-01-27 08:25:31 +0000

    create filtered list

--- slack_extract_query/hello-world/app.ts
+++ slack_extract_query/hello-world/app.ts
@@ -1,22 +1,57 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import {
+    S3Client,
+    ListObjectsV2Command,
+    ListObjectsV2CommandOutput,
+    GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import {Message} from "./models/Message";
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+const region = "eu-west-2";
+const client = new S3Client({region});
+
+const sortMessage = (a: Message, b: Message) => {
+    return Number(a.ts) - Number(b.ts)
+}
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        const command = new ListObjectsV2Command({
+            Bucket: process.env['BUCKET'],
+        });
+        const response: ListObjectsV2CommandOutput = await client.send(command);
+
+        const messages: Message[] = [];
+
+        if (response.Contents) {
+            for (const key of response.Contents) {
+                const getObjectCommand = new GetObjectCommand({
+                    Bucket: process.env['BUCKET'],
+                    Key: key.Key,
+                });
+                const message = await (await client.send(getObjectCommand)).Body?.transformToString();
+                if (message) {
+                    const messageList: Message[] = JSON.parse(message);
+                    for (const messageItem of messageList) {
+                        messages.push(messageItem);
+                    }
+                }
+            }
+        }
+
+        messages.sort(sortMessage);
+
+        const filteredMessages: unknown[] = []
+        messages.map((message) => {
+            filteredMessages.push({
+                ts: (new Date(Number(message.ts)*1000)).toISOString(),
+                text: message.text,
+            })
+        })
+
         return {
             statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
+            body: JSON.stringify(filteredMessages),
         };
     } catch (err) {
         console.log(err);
---
+++ slack_extract_query/hello-world/models/Message.ts
@@ -0,0 +1,12 @@
+export type Message = {
+  type: string;
+  subtype: string;
+  text: string;
+  file: unknown;
+  user: string;
+  upload: boolean;
+  display_as_bot: boolean;
+  username: string;
+  bot_id: string | null;
+  ts: string;
+}

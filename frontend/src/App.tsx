3aa48125761eb77226c48a4071f922e95b6d74f5
ddunford <48522654+darrendunford@users.noreply.github.com>
2023-01-28 09:35:33 +0000

    basic frontend table display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {SlackItem} from "./models/SlackItem";
+import Table from 'react-bootstrap/Table';
+
+interface Params {
+  baseUrl: string;
+  headers: any;
+  method: string;
+}
+
+const getConfig: Params = {
+  baseUrl: "https://reowwnul6h.execute-api.eu-west-2.amazonaws.com/Prod/",
+  headers: {
+    Authorization: undefined
+  },
+  method: "get",
+}
+
+export const getApi = async (url: string): Promise<any> => {
+  return await axios({
+    ...getConfig,
+    url: `${getConfig.baseUrl}/${url}`,
+  }).then((response)=> {
+    console.log(response)
+    return {
+      status: response.status,
+      data: response.data,
+    }
+  }).catch((error) => {
+    console.log(error)
+    return {
+      status: error.status,
+      data: error.response,
+    }
+  })
+}
 
 function App() {
+
+  const [data, setData] = React.useState([])
+  const getData = () => getApi("hello").then((res) => {
+    if (res.status === 200) {
+      setData(res.data)
+      console.log(data)
+    } else {
+      console.log(res)
+    }
+  })
+
+  React.useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th style={{width: '200px'}}>Date/Time</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item: SlackItem) =>
+              <tr>
+                <td>{item.ts.toString()}</td>
+                <td>{item.text.toString()}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </Table>
     </div>
   );
 }
---
+++ frontend/src/models/SlackItem.ts
@@ -0,0 +1,4 @@
+export type SlackItem = {
+  text: string;
+  ts: string;
+}
